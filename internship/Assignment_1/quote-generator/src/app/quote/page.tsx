7fb344780d80a6301c0f05a1aa9befd810ad7f80
Sana-Fatima398 <sanafatima6731@gmail.com>
2025-07-04 16:56:00 +0500

    Added Client Component

--- internship/Assignment_1/quote-generator/src/app/quote/page.tsx
+++ internship/Assignment_1/quote-generator/src/app/quote/page.tsx
@@ -1,14 +1,25 @@
+"use client";
+
 import React from 'react';
+import { useState } from 'react';
+
 export default function Generate(){
     const qouteArray = ["Live Well", "Stay Healthy", "Be Happy", "Work Hard", "Play Hard", "Love Deeply", "Laugh Often", "Dream Big", "Stay Positive", "Embrace Change"];
+    const [quote, setQuote] = useState("");
     const randomIndex = Math.floor(qouteArray.length*Math.random());
+   
+    
     return (
         <div>
             <div><h1>Click the button to Genrate a quote</h1></div>
-            <div><button>CLick me</button></div>
-            <div><h2>{qouteArray[randomIndex]}</h2></div>
+            
+             <button onClick={()=>setQuote(qouteArray[randomIndex])}>Click me</button>
+             <h2>{quote}</h2>
 
         </div>);
         
 }
+
+
+
 
