cd0d265c0c9134a854692b3dc55e61dd76e392df
Sana-Fatima398 <sanafatima6731@gmail.com>
2025-07-04 22:58:09 +0500

    Added ShadCn UI

--- internship/Assignment_1/quote-generator/src/app/quote/page.tsx
+++ internship/Assignment_1/quote-generator/src/app/quote/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import React from 'react';
+import { Button } from '@/components/ui/button';
+import { Card, CardHeader, CardTitle, CardDescription, CardContent } from '@/components/ui/card';
 import { useState } from 'react';
 
 export default function Generate(){
@@ -10,12 +12,27 @@ export default function Generate(){
    
     
     return (
-        <div>
-            <div><h1>Click the button to Genrate a quote</h1></div>
+        <div className="flex items-center justify-center flex-col">
+            <div className='h-20'></div>
+            <div >
+                <Card className='h-72 w-96'>
+                    <CardHeader className='text-center'>
+                        <CardTitle>Quote Generator</CardTitle>
+                        <CardDescription>Click the button to Genrate a quote</CardDescription>
+                        
+                    </CardHeader>
+                    <CardContent className='text-center'>
+                        <div className="m-2">
+                            <Button className="w-32" onClick={()=>setQuote(qouteArray[randomIndex])}>Click me</Button>
+                        </div>
+                        <div className="m-8">
+                            <h2>{quote}</h2>
+                        </div>
+                    </CardContent>
+                   
+                </Card>
+            </div>
             
-             <button onClick={()=>setQuote(qouteArray[randomIndex])}>Click me</button>
-             <h2>{quote}</h2>
-
         </div>);
         
 }
