4d772df2f53bb026d90e5682c68f0f4d4d21d621
Sana-Fatima398 <sanafatima6731@gmail.com>
2025-07-06 20:34:56 +0500

    Changes background

--- internship/Assignment_1/quote-generator/src/app/page.tsx
+++ internship/Assignment_1/quote-generator/src/app/page.tsx
@@ -7,7 +7,7 @@ import { useState } from 'react';
 
 export default function Generate(){
     const [bg, setBg] = useState("");
-    const bgArray = ["1.jpg","2.jpg","3.jpg"];
+    const bgArray = ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpeg","6.avif"];
     const qouteArray = ["Live Well", "Stay Healthy", "Be Happy", "Work Hard", "Play Hard", "Love Deeply", "Laugh Often", "Dream Big", "Stay Positive", "Embrace Change"];
     const [quote, setQuote] = useState("");
    
@@ -20,22 +20,23 @@ export default function Generate(){
 
     return (
         
-        <div className="flex items-center  flex-col bg-cover h-screen bg-fade " 
+        <div className="flex items-center flex-col bg-cover h-screen bg-fade animate-fade" 
                     style={{ backgroundImage: `url(${bg})` }}>
             <div className='h-20'></div>
             <div >
+              
                 <Card className='h-72 w-96'>
                     <CardHeader className='text-center'>
-                        <CardTitle>Quote Generator</CardTitle>
-                        <CardDescription>Click the button to Genrate a quote</CardDescription>
+                        <CardTitle className='text-2xl'>Quote Generator</CardTitle>
+                        <CardDescription>Click the button to Generate a quote</CardDescription>
                         
                     </CardHeader>
                     <CardContent className='text-center'>
                         <div className="m-2">
-                            <Button className="w-32" onClick={MakeChanges}>Click me</Button>
+                            <Button className="w-32 h-10" onClick={MakeChanges}>Generate</Button>
                         </div>
                         <div className="m-8 "  >
-                            <h2 key={quote} className='text-4xl animate-fade'>{quote}</h2>
+                            <h2 key={quote} className='text-4xl italic animate-fade'>{quote}</h2>
                         </div>
                     </CardContent>
                    
